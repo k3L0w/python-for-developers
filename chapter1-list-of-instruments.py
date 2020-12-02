84504913ef01716280040e05f45bd08c0bbcd4f2
@k3l0w <antonio.gerardo.pereira@gmail.com>
2020-12-02 10:54:15 -0300

    Chapter1 - A list of musical instruments

---
+++ chapter1-list-of-instruments.py
@@ -0,0 +1,7 @@
+# A list of musical instruments
+instruments = ['bass', 'drums', 'guitar', 'keyboard']
+print(instruments)
+
+# Percussion musical instruments
+percussion = ['adufe', 'afoxé', 'agogô', 'atabaque', 'batá', 'bateria', 'bloco sonoro', 'bombo', 'bongó', 'caixa Surso', 'carrilhão', 'castanhola', 'caxixi', 'ceramofone', 'chocalho', 'conga', 'cordofone', 'cuica', 'ganza', 'gongo', 'marimba', 'metalofone', 'pandeireta', 'pandeiro', 'pratos', 'reco-reco', 'repinique', 'sino', 'tambom', 'tamborim', 'tantã', 'timpano', 'triângulo', 'vibrafone', 'xequerê', 'xilofone']
+print(percussion)
