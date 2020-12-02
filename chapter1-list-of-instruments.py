56b6b8d722a5584cb28e762f9d89e978487719d1
@k3l0w <antonio.gerardo.pereira@gmail.com>
2020-12-02 11:38:55 -0300

    Atualização da lista de Instrumentos de Percussão

--- chapter1-list-of-instruments.py
+++ chapter1-list-of-instruments.py
@@ -1,7 +1,24 @@
-# A list of musical instruments
+# Uma lista de instrumentos musicais
 instruments = ['bass', 'drums', 'guitar', 'keyboard']
+print()
+print('------------')
+print('instruments:')
+print('------------')
 print(instruments)
+print()
 
-# Percussion musical instruments
-percussion = ['adufe', 'afoxé', 'agogô', 'atabaque', 'batá', 'bateria', 'bloco sonoro', 'bombo', 'bongó', 'caixa Surso', 'carrilhão', 'castanhola', 'caxixi', 'ceramofone', 'chocalho', 'conga', 'cordofone', 'cuica', 'ganza', 'gongo', 'marimba', 'metalofone', 'pandeireta', 'pandeiro', 'pratos', 'reco-reco', 'repinique', 'sino', 'tambom', 'tamborim', 'tantã', 'timpano', 'triângulo', 'vibrafone', 'xequerê', 'xilofone']
-print(percussion)
+# Instrumentos de percussão
+print()
+percussions = ['adufe', 'afoxé', 'agogô', 'atabaque', 'batá', 'bateria', 'bloco sonoro', 'bombo', 'bongó', 'caixa Surso', 'carrilhão', 'castanhola', 'caxixi', 'ceramofone', 'chocalho', 'conga', 'cordofone', 'cuica', 'ganza', 'gongo', 'marimba', 'metalofone', 'pandeireta', 'pandeiro', 'pratos', 'reco-reco', 'repinique', 'sino', 'tambom', 'tamborim', 'tantã', 'timpano', 'triângulo', 'vibrafone', 'xequerê', 'xilofone']
+print('Instrumentos musicais de percussão:')
+print('Se obtém o som por meio da vibração de uma membrana ou superfície.')
+for percussion in percussions:
+    print('.', percussion)
+print()
+
+# Wind musical instruments
+print()
+wind = ['']
+print('------')
+print('wind:')
+print('------') 
