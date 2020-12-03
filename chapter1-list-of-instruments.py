3449f4f0f9dd98a3bc439e8ab02d5ffc7505617b
@k3l0w <antonio.gerardo.pereira@gmail.com>
2020-12-03 08:16:48 -0300

    Adição de listas de outras categorias de instrumentos musicais

--- chapter1-list-of-instruments.py
+++ chapter1-list-of-instruments.py
@@ -1,4 +1,4 @@
-# Uma lista de instrumentos musicais
+# Uma lista de instrumentos musicais comuns
 instruments = ['bass', 'drums', 'guitar', 'keyboard']
 print()
 print('------------')
@@ -7,18 +7,49 @@ print('------------')
 print(instruments)
 print()
 
+print()
+print("Instrumentos musicias são categorizados em três grandes grupos:")
+print("1. percussão")
+print("2. sopro")
+print("3. cordas")
+
 # Instrumentos de percussão
 print()
 percussions = ['adufe', 'afoxé', 'agogô', 'atabaque', 'batá', 'bateria', 'bloco sonoro', 'bombo', 'bongó', 'caixa Surso', 'carrilhão', 'castanhola', 'caxixi', 'ceramofone', 'chocalho', 'conga', 'cordofone', 'cuica', 'ganza', 'gongo', 'marimba', 'metalofone', 'pandeireta', 'pandeiro', 'pratos', 'reco-reco', 'repinique', 'sino', 'tambom', 'tamborim', 'tantã', 'timpano', 'triângulo', 'vibrafone', 'xequerê', 'xilofone']
-print('Instrumentos musicais de percussão:')
-print('Se obtém o som por meio da vibração de uma membrana ou superfície.')
+print("Instrumentos musicais de percussão:")
+print("Nesses instrumentos se obtém o som por meio da vibração de uma membrana ou superfície.")
 for percussion in percussions:
     print('.', percussion)
 print()
 
-# Wind musical instruments
+# Instrumentos de sopro (Madeira)
 print()
-wind = ['']
-print('------')
-print('wind:')
-print('------') 
+madeiras = ['Clarinete', 'clarone', 'contrafagote', 'corne', 'didjeridu', 'fagote', 'flauta', 'flautim', 'gaita', 'gaita de foles', 'harmônica', 'oboé', 'requinta', 'saxofone']
+print("Instrumentos musicais de sopro constituídos de madeira")
+print("O som é obtido por meio da movimentação do ar nos tubos que constituem esses instrumentos")
+for madeira in madeiras:
+    print('.', madeira)
+print()
+
+# Instrumentos de sopro (metal)
+print()
+metais = ["bombardino", "clarim", "corneta", "fliscorne", "trombone", "trompa", "trompete", "tuba"]
+print("Instrumentos musicais de sopro constituídos de metal")
+for metal in metais:
+    print(".", metal)
+print()
+
+# Instrumentos musicais de corda
+print()
+cordas = ["alaúde", "baixo", "balalaica", "bandola", "bandolim", "banjo", "berimbau", "cavaquinho", "charango", "citara", "contrabaixo", "cordofone", "guitarra", "harpa", "rabeca", "ukulele", "viola", "violão", "violino", "violoncelo"]
+print("O som é obtido por meio da movimentação do ar nos tubos que constituem esses instrumentos")
+for corda in cordas:
+    print(".", corda)
+print()
+
+# Instrumentos musicais de teclas se classificam como instrumentos de cordas.
+print()
+teclas = ["acordeão", "clavicórdio", "concertina", "cravo", "espineta", "órgão", "piano", "sintetizador", "teclado"]
+for tecla in teclas:
+    print(".", tecla)
+print()
