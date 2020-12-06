a0777f60f8a3ad8ab52f5863efc2a2e68ec669c2
@k3l0w <antonio.gerardo.pereira@gmail.com>
2020-12-06 09:28:35 -0300

    Novo arquivo: Python como calculadora

---
+++ chapter1-python-como-calculadora.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+###############################
+# PYTHON COMO UMA CALCULADORA #
+###############################
+
+print()
+print("-------------------------------")
+print("* PYTHON COMO UMA CALCULADORA *")
+print("-------------------------------")
+print()
+print("AS QUATROS OPERAÇÕES BÁSICA DA MATEMÁTICA:")
+print("Usamos os símbolos: +, -, * e / para soma, subtração, multiplicação e divisão, veja: ")
+print()
+print("Adição:")
+print("11 + 4 + 5 =", 11 + 4 + 5)
+print()
+print("Subtração:")
+print("11 - 4 - 5 =", 11 - 4 - 5)
+print()
+print("Multiplicação:")
+print("4 * 3 =", 4 * 3)
+print()
+print("Divisão:")
+print("12 / 5 =", 12 / 5)
+print()
+
+print("Potenciação:")
+print("Potenciação se usa **, para calcular 2³, ficará: 2 ** 3, veja só:")
+print("2³ =", 2 ** 3)
+
+print()
+print("Divisão inteira e resto:")
+print("Nesse tipo de operação, um número é dividido por outro e depois o resultado é ARREDONDADO para baixo.")
+print("Se usa: // para efetuar a operação.")
+print("3 // 2 =", 3 // 2)
+
+print()
+print("Cálculo do resto da divisão de um número por outro:")
+print("17 % 5 =", 17 % 5)
+print("----------------------------------------------")
+print()
+
+print("Abaixo, temos alguns exercícios de exemplo:")
+print("1. Fany comprou uma TV WebOS no valor de R$ 2099,00, sdividiu em 12 vezes sem juros.")
+print("   Qual o valor de cada parcela?")
+print("   Resposta:", 2099 / 12)
+print()
+
+print("2. Meire fez dez anos de idade em 6 de dezembro de 2020. Calcule quantidades de dias viveu nesse perído.")
+print("   Meire nasceu em 6 de dezembro de 2010, logo, Meire viveu, nesse período:", 10 * 365, "dias.")
+
