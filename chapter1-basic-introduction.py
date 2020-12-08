5d34c8481e29bd613ad5d75f0650af562cc2d82a
@k3l0w <antonio.gerardo.pereira@gmail.com>
2020-12-08 07:42:51 -0300

    Adição de arquivo: chapter1-basic-introduction.py

---
+++ chapter1-basic-introduction.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#Strings no python são definidas por aspas duplas ("") ou simples ('').
+#vide:
+#>>> "digite_texto_aqui"
+# ou
+#>>> 'digite_texto_aqui'
+
+#Função .upper()
+#Esta função retorna CAIXA ALTA para as strings.
+#vide:
+#>>> "texto".upper()
+#TEXTO
+#>>>
+
+#Função len()
+#Retorna a quantidade de caracteres em uma ou mais strings.
+#vide:
+#>>> len("A função len irá retornar a quantidade de caracteres, incluindo os espaços que esta string possue.")
+#98
+#>>>
+
+#Função str
+#Converte números em strings.
+#vide:
+#>>>len(str(123))
+#123
+#>>>len('123')
+#123
+#>>>
+
+#Função int
+#Converte números (não inteiros) em números inteiros.
+#int(123,45)
+#123
+#>>>
+
+#Variáveis no Python
+#É de suma importância o uso de variáveis, pois é um nome que se atribui a um objeto.
+#Variáveis podem ser utilizadas para armazenar dados, isso torna o código mais legível e muito mais produtivo.
+#Vide:
+#name = "Arham Om Talsania" #Talsania, indiano de 6 anos, reconhecido como o programador mais jovem do mundo.
+#>>> name
+#Arham Om Talsania
