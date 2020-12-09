4b0bc15f46a26b91356e8026228e99db492c771b
@k3l0w <antonio.gerardo.pereira@gmail.com>
2020-12-09 07:27:17 -0300

    Atualizacao-e-criacao-de-arquivo-1208

--- chapter1-basic-introduction.py
+++ chapter1-basic-introduction.py
@@ -40,6 +40,22 @@
 #É de suma importância o uso de variáveis, pois é um nome que se atribui a um objeto.
 #Variáveis podem ser utilizadas para armazenar dados, isso torna o código mais legível e muito mais produtivo.
 #Vide:
+#Variável pode armazenar texto:
 #name = "Arham Om Talsania" #Talsania, indiano de 6 anos, reconhecido como o programador mais jovem do mundo.
 #>>> name
 #Arham Om Talsania
+
+#Utilizando funções com strings
+#>>>name = "Arham Om Talsania"
+#>>>len(name)
+#17
+#>>>
+
+#Variável pode armazenar números:
+#>>> a = 7
+#>>> b = 5
+#>>> a * b
+#35
+
+#Função print no Python
+
---
+++ tipos-d-erros.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+#---------
+#TypeError
+#---------
+#Vide exemplo:
+#>>> len(123456)
+#Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+#TypeError: object of type 'int' has no len()
+#>>>
+
+
+#----------
+#name Error
+#----------
+#Vide exemplo:
+#>>> city = "Sao Paulo"
+#>>> citi
+#Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+#NameError: name 'citi' is not defined
+#>>>
