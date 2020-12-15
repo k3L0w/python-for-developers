025bef4f81513da7a14ed47751d7745c9047b931
@k3l0w <antonio.gerardo.pereira@gmail.com>
2020-12-15 07:49:58 -0300

    Chapter2 - Code study 1512

---
+++ chapter2-sintaxe.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+
+# Este é um comentário, qualquer frase ou texto podem ser 'comentadas' em meio a códigos.
+
+print()
+print(7 * 3)
+
+# Em python podemos utilizar o caracter '\' para dividir o código em linhas.
+# Vide:
+
+# Uma linha quebrada por contra-barra
+a = 7 * 3 + \
+    5 / 2
+print(a)
+
+# Uma lista quebrada por vírgula
+b = ['a', 'b','c',
+'d', 'e']
+print(b)
+
+# Uma chamada de função (quebrada por vírgula)
+# min() retorna o menor elemento de uma sequẽncia
+c = min(1, 2, 5, 7,
+11)
+print(c)
+print()
