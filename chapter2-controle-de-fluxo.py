3ea858e3c124321babf03ec978257a71e76c77b8
@k3l0w <antonio.gerardo.pereira@gmail.com>
2020-12-16 08:05:17 -0300

    chapter2 - Code Study 1612Update

--- chapter2-controle-de-fluxo.py
+++ chapter2-controle-de-fluxo.py
@@ -18,7 +18,14 @@ else:
 temp = int(input('Entre com a temperatura atual: '))
 
 if temp < 0:
-    print('Congelando...')
+    print()
+    print('CUIDADO COM VENTOS! Temperaturas abaixo de zero (0 )oferecem risco a vida!')
+    print('Em temperaturas de -29° com ventos a 16km/h, a sensação térmica seria equivalente a -44°C')
+    print('Em temperaturas de -29, com ventos a 40Km/h, a sensação térmica seria equivalente a -66°C')
+    print()
+    print('CUIDADO COM A ÁGUA, a sensação térmica pode ser ainda pior!!!.')
+    print('Um mergulho em um lago a -5°C pode causar hipotermia em menos de 30 minutos.')
+    print()
 elif 0 <= temp <= 20:
     print('Frio')
 elif 21 <= temp <= 25:
